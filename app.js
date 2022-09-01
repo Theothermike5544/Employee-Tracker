a4e45b2df9a7b4a77c0346753ebd9d062ee020d6
Michael Bishop <mrbishop5544@gmail.com>
2022-09-01 17:51:43 -0400

    added required functions and inquirer ?'s, functions working!

--- app.js
+++ app.js
@@ -1,82 +1,83 @@
-const mysql = require('mysql');
-const inquirer = require('inquirer');
-const consTable = require('console.table');
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
-    host: 'localhost',
+    host: "localhost",
 
     // Your port; if not 3306
     port: 3306,
-
+  
     // Your username
-    user: 'root',
-
+    user: "root",
+  
     // Be sure to update with your own MySQL password!
-    password: 'password',
-    database: 'company_db',
+    password: "password",
+    database: "company_db",
 });
 
-  //initial start function to call other function
+//initial start function to call other function
 const start = () => {
-    inquirer 
-    .prompt([{
-        name: 'options',
-        type: 'list', 
-        message: 'Choose one of the options below.',
+    inquirer
+    .prompt([
+        {
+        name: "options",
+        type: "list",
+        message: "Choose one of the options below.",
         choices: [
-            "View all Employees?", 
+            "View all Employees?",
             "View all Roles?",
-            "View all Department's", 
+            "View all Department's",
             "Update Employee roles",
             "Add Employee?",
             "Add Role?",
-            "Add Department?"
-        ]
-    }])
+            "Add Department?",
+          ],
+        },
+    ])
 
     .then((answer) => {
         switch (answer.options) {
             case "View all Employees?":
               viewAllEmploy();
-            break;
-
+              break;
+    
             case "View all Roles?":
               viewRoles();
-            break;
-
+              break;
+    
             case "View all Department's":
               viewDepart();
-            break;
-
+              break;
+    
             case "Update Employee roles":
-                updateEmployRoles();
-            break;
-
+              updateEmployRoles();
+              break;
+    
             case "Add Employee?":
-                addEmployee();
-            break;
-
+              addEmployee();
+              break;
+    
             case "Add Role?":
-                addRole();
-            break;
-
+              addRole();
+              break;
+    
             case "Add Department?":
-                addDept();
-            break;
+              addDept();
+              break;
         }
-    }) 
+    });
 };
 
 //View all employees
 
 const viewAllEmploy = () => {
 
-connection.query("Select * FROM employee"), (err, res) => {
-    if(err) throw err;
-    console.log(res);
-    console.table(res);
-    start();
-}
+    connection.query("SELECT * FROM employee;", (err, res) => {
+        if (err) throw err;
+        console.table(res);
+        start();
+    });
 
 };
 
@@ -84,26 +85,28 @@ connection.query("Select * FROM employee"), (err, res) => {
 
 const viewRoles = () => {
 
-    connection.query("Select * FROM role"), (err, res) => {
-        if(err) throw err;
+    connection.query("Select * FROM roles", (err, res) => {
+        if (err) throw err;
         console.table(res);
-        console.log(res);
         start();
-    }
-
+    });
 };
 
 //View all roles
 
 const viewDepart = () => {
 
-    connection.query("Select * FROM department"), (err, res) => {
-        if(err) throw err;
+    connection.query("Select * FROM department", (err, res) => {
+        if (err) throw err;
         console.table(res);
-        console.log(res);
         start();
-    }
+    });
 };
+
+const addEmployee = () => {
+    connection.query("SELECT * FROM roles", (err, roles) => {
+      if (err) throw err;
+      let newRoles = roles.map((role) => ({ name: role.title, value: role.id }));
 
 //Select Role Quieries Role Title for Add Employee Prompt
 roles = []
@@ -117,58 +120,179 @@ const selectRole = () => {
     return roles;
 };
 
-//Select Role Quieries The managers for Add Employee Prompt
-manager = []
-const selectManager = () => {
-    connection.query("SELECT * FORM role"), (err, res) => {
-        if(err) throw err;
-        for (let i = 0; i < res.length; i++) {
-            manager.push(res[i].first_name);
-        }
-    }
-    return manager;
+connection.query("SELECT * FROM employee", (err, managers) => {
+    if (err) throw err;
+    let newManager = managers.map((manager) => ({
+      name: `${manager.first_name} ${manager.last_name}`,
+      value: manager.id,
+    }));
+    inquirer
+      .prompt([
+        {
+          name: "firstName",
+          type: "input",
+          message: "Please enter employees first name.",
+        },
+        {
+          name: "lastName",
+          type: "input",
+          message: "Please enter employees last name.",
+        },
+        {
+          name: "role",
+          type: "rawlist",
+          message: "What is the employees role?",
+          choices: newRoles,
+        },
+        {
+          name: "manager",
+          type: "rawlist",
+          message: "What is the employees managers name?",
+          choices: newManager,
+        },
+      ])
+      .then((answer) => {
+        connection.query(
+          "INSERT INTO employee SET ?",
+          {
+            first_name: answer.firstName,
+            last_name: answer.lastName,
+            manager_id: answer.manager,
+            role_id: answer.role,
+          },
 
+          (err, res) => {
+            if (err) throw err;
+            console.log(`${res.affectedRows} employee inserted!\n`);
+            // Call start AFTER the INSERT completes
+            start();
+          }
+        );
+      });
+  });
+});
 };
 
-const addEmployee = () => {
+const addRole = () => {
+    connection.query("SELECT * FROM department", (err, departments) => {
+        if (err) throw err;
+        let newDepartment = departments.map((department) => ({
+          name: `${department.name}`,
+          value: department.id,
+        }));
+        inquirer
+        .prompt([
+          {
+            name: "roleName",
+            type: "input",
+            message: "Please enter your Role name.",
+          },
+          {
+            name: "salary",
+            type: "input",
+            message: "Please enter salary.",
+          },
+          {
+            name: "department",
+            type: "rawlist",
+            message: "What department is your role in?",
+            choices: newDepartment,
+          },
+        ])
+        .then((answer) => {
+          connection.query(
+            "INSERT INTO roles SET ?",
+            {
+              title: answer.roleName,
+              salary: answer.salary,
+              department_id: answer.department
+            },
 
-    inquirer 
-    .prompt([
-    {
-        name: 'firstName',
-        type: 'input', 
-        message: 'Please enter employees first name.'
-    },
-    {
-        name: 'lastName',
-        type: 'input',
-        message: 'Please enter employees last name.'
-    },
-    {
-        name: 'role',
-        type: 'list',
-        message: 'What is the employees role?',
-        choices: selectRole() 
-    },
-    {
-        name: 'manager',
-        type: 'list',
-        message: 'What is the employees managers name?',
-        choices: selectManager() 
-    }
-    ])
-    .then((answer) => {
-        console.log(answer);
-    })
+            (err, res) => {
+              if (err) throw err;
+              console.log(`${res.affectedRows} role inserted!\n`);
+              // Call start AFTER the INSERT completes
+              start();
+            }
+          );
+        });
+
+    });
 };
 
-// const addRole = () => {
+const addDept = () => {
+    inquirer
+        .prompt([
+          {
+            name: "newDept",
+            type: "input",
+            message: "Please enter your New Department name.",
+          },
+        ])
+        .then((answer) => {
+          connection.query(
+            "INSERT INTO department SET ?",
+            {
+              name: answer.newDept,
+            },
+            (err, res) => {
+                if (err) throw err;
+                console.log(`${res.affectedRows} department inserted!\n`);
+                // Call start AFTER the INSERT completes
+                start();
+            }
+        );
+    });
+};
 
-// };
+const updateEmployRoles = () => {
+    connection.query("SELECT * FROM roles", (err, roles) => {
+        if (err) throw err;
+        let newRoles = roles.map((role) => ({ name: role.title, value: role.id }));
 
-// const addDept = () => {
-
-// };
+        connection.query("SELECT * FROM employee", (err, employees) => {
+          if (err) throw err;
+          let newEmployee = employees.map((employee) => ({
+            name: `${employee.first_name} ${employee.last_name}`,
+            value: employee.id,
+          }));
+          inquirer
+            .prompt([
+              {
+                name: "employee",
+                type: "rawlist",
+                message: "Which employee do you want to update?",
+                choices: newEmployee,
+              },
+              {
+                name: "role",
+                type: "rawlist",
+                message: "What is the employees new role?",
+                choices: newRoles,
+              },
+            ])
+            .then((answer) => {
+            connection.query(
+                "UPDATE employee SET ? WHERE ?",
+                [
+                {
+                  role_id: answer.role,
+                },
+                {
+                  id: answer.employee,  
+                }
+                ],
+                (err, res) => {
+                  if (err) throw err;
+                  console.log(`${res.affectedRows} new role's inserted!\n`);
+                  // Call start AFTER the INSERT completes
+                  start();
+                }
+            );
+            });
+        });
+    });
+}
 
 // connect to the mysql server and sql database
 connection.connect((err) => {
